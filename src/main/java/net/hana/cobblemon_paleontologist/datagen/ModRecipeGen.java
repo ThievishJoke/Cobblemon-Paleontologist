cbb2082d855d0efcd0da542fb8445f5cdee58484
ThievishJoke <hanayokoikimo@gmail.com>
2025-03-24 18:43:38 -0400

    New Textures New Structures

--- src/main/java/net/hana/cobblemon_paleontologist/datagen/ModRecipeGen.java
+++ src/main/java/net/hana/cobblemon_paleontologist/datagen/ModRecipeGen.java
@@ -6,7 +6,6 @@ import net.hana.cobblemon_paleontologist.block.ModBlocks;
 import net.minecraft.block.Blocks;
 import net.minecraft.data.server.recipe.RecipeExporter;
 import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
-import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.ItemTags;
@@ -22,14 +21,11 @@ public class ModRecipeGen extends FabricRecipeProvider {
     @Override
     public void generate(RecipeExporter exporter) {
         ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModBlocks.LAB_TABLE)
-                .pattern("OB ")
-                .pattern("SSS")
-                .pattern("FVF")
+                .pattern("   ")
+                .pattern("SS ")
+                .pattern("FF ")
                 .input('S', Blocks.DEEPSLATE)
                 .input('F', ItemTags.PLANKS)
-                .input('B', Items.BRUSH)
-                .input('O', Items.BREWING_STAND)
-                .input('V', Items.STICK)
                 .criterion(hasItem(Blocks.DEEPSLATE), conditionsFromTag(ItemTags.PLANKS))
                 .offerTo(exporter);
     }
