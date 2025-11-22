7032348b1daba44b7f710f267433d6645845ca11
ThievishJoke <hanayokoikimo@gmail.com>
2025-11-21 21:26:16 -0500

    seperate minigame from jei/emi compat

--- src/main/java/net/hana/cobblemon_paleontologist/compat/jei/FossilProcessingRecipe.java
+++
@@ -1,8 +0,0 @@
-package net.hana.cobblemon_paleontologist.compat.jei;
-
-import net.minecraft.item.ItemStack;
-
-import java.util.List;
-
-public record FossilProcessingRecipe(ItemStack input, List<ItemStack> outputs) {
-}
